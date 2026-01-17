1a344a93887c6767c94216aef6357adcf5f1069a
ahadlakhani95 <ahadlakhani95@gmail.com>
2026-01-17 19:38:25 +0530

    responsive css added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,41 @@
 import React from 'react'
 import './hero.css'
 import { FaLocationDot } from "react-icons/fa6";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import Typewriter from './Typewriter';
 import img1 from './../assets/ahad.jpg'
 
 const Hero = () => {
     return (
         <div id='home'>
-            {/* <div>
-               
-            </div>
-            <div className='Location'>
-             
-            </div> */}
             <div className="hero">
                 <div className="hero_part1">
-                    <h1>Hey , I'm <span className='color'>Ahad</span> </h1>
-                    <h3>  <FaLocationDot /> Bhavnagar,Gujarat</h3>
+                    <h1>Hey , I'm <span className='color'>Ahad</span></h1>
+                    <h3><FaLocationDot /> Bhavnagar, Gujarat</h3>
 
                     <Typewriter />
 
+                    {/* Social Icons */}
+                    <div className="hero_socials">
+                        <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
+                            <FaGithub className="social_icon" />
+                        </a>
+
+                        <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer">
+                            <FaLinkedin className="social_icon" />
+                        </a>
+                    </div>
+
+                    {/* Resume Button */}
+                    <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+                        <button className="resume_btn">Download Resume</button>
+                    </a>
                 </div>
+
                 <div className="hero_part2">
                     <div className="hero_img"></div>
                 </div>
             </div>
-
-
         </div>
     )
 }
